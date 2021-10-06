331d19f8033e35884143a9b9eaa7fbbf15f56604
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-06 17:22:36 +0200

    Playing with class & counters

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,58 @@
-import Head from 'next/head'
-import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
+import React, { useState } from "react";
+
+// Simple function component
+const CompA = () => {
+  return (
+    <>
+      <h1>Hi Comp A</h1>
+      <CompC />
+    </>
+  );
+};
+
+// Class component with state
+class CompC extends React.Component {
+  state = {
+    classValue: 15,
+  };
+
+  incValue(inc) {
+    let newValue = this.state.classValue + inc;
+    this.setState({
+      classValue: newValue,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Comp C</h1>;<h1>Current Value : {this.state.classValue}</h1>
+        <button type="button" onClick={() => this.incValue(+1)}>
+          Incrementer
+        </button>
+        <button type="button" onClick={() => this.incValue(-1)}>
+          Decrementer
+        </button>
+      </>
+    );
+  }
+}
 
 export default function Home() {
+  const [value, setValue] = useState(0);
+
+  const incValue = (inc) => setValue((value) => value + inc);
+
   return (
-    <h1>Hello NeXt</h1>
-  )
+    <>
+      <h1>Current Value : {value}</h1>
+      <button type="button" onClick={() => incValue(+1)}>
+        Incrementer
+      </button>
+      <button type="button" onClick={() => incValue(-1)}>
+        Decrementer
+      </button>
+      <CompC />
+    </>
+  );
 }
