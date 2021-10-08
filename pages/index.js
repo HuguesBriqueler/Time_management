efd7e947120f25b53279ad3a8ba1cb1484e70eb0
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-08 16:23:33 +0200

    Serving local data resources to components

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,18 @@ import RessourceHighlight from "components/ResourceHighlight"
 import Newsletter from "components/Newsletter"
 import ResourceList from "components/ResourceList"
 import Footer from "components/Footer"
+import { resources } from "api/data"
 
 export default function Home() {
   return (
     <Layout>
-      <RessourceHighlight />
+      <RessourceHighlight 
+        resources = {resources.slice(0, 2)} // Only 2 first elements will be sent
+      />
       <Newsletter />
-      <ResourceList />
+      <ResourceList 
+        resources = {resources}
+      />
       <Footer />
     </Layout>
   )
