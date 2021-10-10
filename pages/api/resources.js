d8ce0b9bf1346177880962d6a936e5189a3cedef
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-10 20:03:29 +0200

    some comments

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,4 +1,10 @@
 
-export default function(req, res) {
-  res.send("** WORK IN PROGRESS **")
+// From here (server-side) no CORS limitation
+// External API server can be fetched
+
+export default async function(req, res) {
+  const resData = await fetch("http://localhost:3001/api/resources")
+  const data = await resData.json()
+
+  res.send(data)
 }
