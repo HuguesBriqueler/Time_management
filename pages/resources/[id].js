2f8cf748163840366ff8dd16c5b911eff4a4b9e7
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-13 01:57:51 +0200

    Changed function getServersideProps to getStaticProps & getStaticPaths

--- pages/resources/[id].js
+++ pages/resources/[id].js
@@ -24,7 +24,22 @@ const resourceDetail = ({ resource }) => {
   );
 };
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+  // Here are gathered each page id, then, at build time, a page will be generated for each id (path)
+  const resData = await fetch("http://localhost:3001/api/resources")
+  const data = await resData.json()
+  const paths = data.map(resource => {
+    return {
+      params: {id: resource.id}
+    }
+  })
+  return {
+    paths,  // equal to --> paths: paths
+    fallback: false  // other paths will lead to 404 page
+  }
+}
+
+export async function getStaticProps({ params }) {
   // params refers to id parameter ('/resources/id') set in file name ('[id].js')
   // query refers to 'id' and other 'query parameters' such as '/resources/456?someQuery=hello'
 
@@ -39,5 +54,21 @@ export async function getServerSideProps({ params }) {
     },
   };
 }
+
+// export async function getServerSideProps({ params }) {
+//   // params refers to id parameter ('/resources/id') set in file name ('[id].js')
+//   // query refers to 'id' and other 'query parameters' such as '/resources/456?someQuery=hello'
+
+//   const dataRes = await fetch(
+//     `http://localhost:3001/api/resources/${params.id}`
+//   );
+//   const data = await dataRes.json();
+
+//   return {
+//     props: {
+//       resource: data,
+//     },
+//   };
+// }
 
 export default resourceDetail;
