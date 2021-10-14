12e7aa0b4b71a5b2a9902739fdbfe673da8664ae
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-15 00:28:34 +0200

    Added ActiveResource component + CSS

---
+++ components/ActiveResource.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+function ActiveResource() {
+  return (
+    <div className="active-resource">
+      <h1 className="resource-name">My active resource</h1>
+      <div className="time-wrapper">
+        <h2 className="elapsed-time">
+          1400
+        </h2>
+      </div>
+      <Link href="/">
+        <a className="button">
+          Go to resource
+        </a>
+      </Link>
+    </div>
+  )
+}
+
+export default ActiveResource
--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,11 @@
 import Navbar from "components/Navbar"
+import ActiveResource from "components/ActiveResource"
 
 function Layout({children}) {
   return (
     <>
       <Navbar />
+      <ActiveResource />
       {children}
     </>
   )
