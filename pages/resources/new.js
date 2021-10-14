090fff17440b72642fd8d3a197c2c5542bc2cc1a
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-14 02:45:40 +0200

    ResourceForm component has been refactored from new.js page, in order to be reusable when creating and updating a resource

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,6 +1,6 @@
 import Layout from "components/Layout";
+import ResourceForm from "components/ResourceForm";
 import axios from "axios";
-import { useState } from "react";
 import { useRouter } from "next/dist/client/router";
 
 const DEFAULT_DATA = {
@@ -12,30 +12,15 @@ const DEFAULT_DATA = {
 };
 
 const ResourceCreate = () => {
-  const [form, setForm] = useState(DEFAULT_DATA);
-  
   const router = useRouter()
 
-  const submitForm = () => {
+  // function is called from ResourceForm component with form state as argument
+  const createResource = (formData) => {
     // fetch request is sent to our server-side api
-    axios.post("/api/resources", form)
+    axios.post("/api/resources", formData)
       // .then( res => alert(res.data))
       .then( _ => router.push("/")) // La "variable" _ (underscore) indique qu'il existe un parametre mais qu'il sera ignoré
       .catch( err => alert(err?.response?.data));
-  };
-
-  const resetForm = () => {
-    setForm(DEFAULT_DATA);
-  };
-
-  const handleChange = (e) => {
-    // console.log(`${e.target.name} send ${e.target.value}`);
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-      // [e.target.name]: e.target.value
-    });
   };
 
   return (
@@ -45,96 +30,10 @@ const ResourceCreate = () => {
           <div className="column is-8 is-offset-2">
             <div className="resource-form">
               <h1 className="title">Add new resource</h1>
-              <form>
-                <div className="field">
-                  <label className="label">Title</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="Enter a title"
-                      value={form.title}
-                      name="title"
-                      onChange={handleChange}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Description</label>
-                  <div className="control">
-                    <textarea
-                      className="textarea"
-                      placeholder="Enter description"
-                      value={form.description}
-                      name="description"
-                      onChange={handleChange}
-                    ></textarea>
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Link</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="Enter a link to the resource"
-                      value={form.link}
-                      name="link"
-                      onChange={handleChange}
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Priority</label>
-                  <div className="control">
-                    <div className="select">
-                      <select
-                        value={form.priority}
-                        name="priority"
-                        onChange={handleChange}
-                      >
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Time to finish</label>
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="number"
-                      placeholder="60"
-                      value={form.timeToFinish}
-                      name="timeToFinish"
-                      onChange={handleChange}
-                    />
-                  </div>
-                  <p className="help">Finish time in minutes</p>
-                </div>
-                <div className="field is-grouped">
-                  <div className="control">
-                    <button
-                      type="button"
-                      onClick={submitForm}
-                      className="button is-link"
-                    >
-                      Submit
-                    </button>
-                  </div>
-                  <div className="control">
-                    <button
-                      type="button"
-                      onClick={resetForm}
-                      className="button is-link is-light"
-                    >
-                      Reset form
-                    </button>
-                  </div>
-                </div>
-              </form>
+              <ResourceForm
+                // Here the createResource function is given to ResourceForm as props
+                onFormSubmit={createResource} 
+              />
             </div>
           </div>
         </div>
