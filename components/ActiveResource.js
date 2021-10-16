efe733492dbac1cd143e587bc7880cf13c74e656
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-16 20:58:58 +0200

    Added countdown to remaining time

--- components/ActiveResource.js
+++ components/ActiveResource.js
@@ -5,29 +5,43 @@ import moment from "moment";
 
 function ActiveResource() {
   const [resource, setResource] = useState({});
+  const [seconds, setSeconds] = useState();
 
   useEffect(() => {
     const fetchResource = async () => {
       const axiosRes = await axios.get("/api/activeResource");
       const resource = axiosRes.data;
       const timeToFinish = parseInt(resource.timeToFinish, 10);
-      const elapsedTime = moment().diff(moment(resource.activationTime), "seconds");
-      const updatedTimeToFinish = (resource.timeToFinish * 60) - elapsedTime;
+      const elapsedTime = moment().diff(
+        moment(resource.activationTime),
+        "seconds"
+      );
+      const updatedTimeToFinish = resource.timeToFinish * 60 - elapsedTime;
 
-      if (updatedTimeToFinish >=0 ) {
-        resource.timeToFinish = updatedTimeToFinish
+      if (updatedTimeToFinish >= 0) {
+        resource.timeToFinish = updatedTimeToFinish;
+        setSeconds(updatedTimeToFinish);
       }
-
       setResource(resource);
     };
     fetchResource();
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((seconds) => seconds - 1);
+    }, 1000);
+    if (seconds <= 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [seconds]);
+
   return (
     <div className="active-resource">
       <h1 className="resource-name">{resource.title}</h1>
       <div className="time-wrapper">
-        <h2 className="elapsed-time">{resource.timeToFinish}</h2>
+        <h2 className="elapsed-time">{seconds}</h2>
       </div>
       <Link href="/">
         <a className="button">Go to resource</a>
