8644afcee9244b5119850a068d0dd8ce318b0987
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-10-16 19:29:27 +0200

    Installed moment library as dependency then remaining time display in ActiveResource component

--- components/ActiveResource.js
+++ components/ActiveResource.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import moment from "moment";
 
 function ActiveResource() {
   const [resource, setResource] = useState({});
@@ -9,6 +10,14 @@ function ActiveResource() {
     const fetchResource = async () => {
       const axiosRes = await axios.get("/api/activeResource");
       const resource = axiosRes.data;
+      const timeToFinish = parseInt(resource.timeToFinish, 10);
+      const elapsedTime = moment().diff(moment(resource.activationTime), "seconds");
+      const updatedTimeToFinish = (resource.timeToFinish * 60) - elapsedTime;
+
+      if (updatedTimeToFinish >=0 ) {
+        resource.timeToFinish = updatedTimeToFinish
+      }
+
       setResource(resource);
     };
     fetchResource();
@@ -18,7 +27,7 @@ function ActiveResource() {
     <div className="active-resource">
       <h1 className="resource-name">{resource.title}</h1>
       <div className="time-wrapper">
-        <h2 className="elapsed-time">1400</h2>
+        <h2 className="elapsed-time">{resource.timeToFinish}</h2>
       </div>
       <Link href="/">
         <a className="button">Go to resource</a>
